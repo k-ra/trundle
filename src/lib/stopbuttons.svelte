623aed63e7e8d254e647c82d94b539c39f735e86
k-ra <kyraymo@gmail.com>
2024-03-14 21:40:41 -0400

    day one
    stop buttons, json files, input

---
+++ src/lib/stopbuttons.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    let status = "start";
+    let user_route = new Array(2);
+    let stops = [
+        { stop_id: 1, stop_name: "WIDENER" },
+        { stop_id: 2, stop_name: "QUAD" },
+    ];
+
+    function handleStopClick(stop_id: number) {
+        if (status == "start") {
+            user_route[0] = stop_id;
+            status = "end";
+        } else {
+            user_route[1] = stop_id;
+        }
+    }
+</script>
+
+<h1>input your {status} stop</h1>
+{#each stops as { stop_id, stop_name }}
+    <button class={status} on:click={() => handleStopClick(stop_id)}>
+        {stop_name}
+    </button>
+{/each}
+
+<h2>
+    TESTING!!! start stop id = {user_route[0]}. end stop id = {user_route[1]}
+</h2>
+
+<style>
+    .start {
+        color: green;
+    }
+    .stop {
+        color: red;
+    }
+    button {
+        font-size: 25pt;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,32 @@
 <script lang="ts">
+    import Input from "../lib/stopbuttons.svelte";
 </script>
 
-<h1>trundle time</h1>
+<h1>trundling time</h1>
+<Input />
+<h3>
+    (do NOT call me crusty !!! i am textured) i jus drank milk all over my big
+    chinne
+</h3>
+<h1 id="this">Everything is softer than mym hat.</h1>
+
 <img src="thatguy.jpg" alt="little baby squirrel loves to navigate the world" />
+
+<h2>do you want to know about what is on my Map!</h2>
 
 <style>
     img {
-        width: 800px;
-        height: 800px;
+        width: 500px;
         display: block;
         margin: 0 auto;
+    }
+
+    h2 {
+        text-align: center;
+    }
+
+    #this {
+        font-size: 120px;
+        text-align: center;
     }
 </style>
